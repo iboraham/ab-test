113d705272910e58b070a3ccc41d1243ebe1cf2e
Onur Şerbetçi <onurserbetcii@gmail.com>
2021-06-05 19:15:27 +0100

    package

---
+++ setup.py
@@ -0,0 +1,34 @@
+from distutils.core import setup
+
+setup(
+    name="ab-test",
+    packages=["ab-test"],
+    version="0.1",
+    license="MIT",
+    description="A/B testing framework for python",
+    author="Ibrahim Onur Serbetci",
+    author_email="onurserbetcii@gmail.com",
+    url="https://github.com/iboraham/ab-test",
+    download_url="https://github.com/iboraham/ab-test/archive/v_01.tar.gz",
+    keywords=[
+        "ab-testing",
+        "hypothesis-testing",
+        "z-test",
+    ],
+    install_requires=[
+        "statsmodels",
+        "pandas",
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Data Scientist",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+)
